43c62a56173ec3216da3587622afbd3930c1ed9b
OKAMOTO YUKI <okayu110108@gmail.com>
2022-09-26 21:30:50 +0900

    Create redux

--- client/src/components/layout/AppLayout.jsx
+++ client/src/components/layout/AppLayout.jsx
@@ -1,22 +1,27 @@
 import { Box } from '@mui/material';
 import React from 'react';
 import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { Outlet, useNavigate } from 'react-router-dom';
+import { setUser } from '../../redux/features/userSlice';
 import authUtils from '../../utils/authUtils';
 import Sidebar from '../common/Sidebar';
 
 const AppLayout = () => {
   const navigate = useNavigate();
+  const dispatch = useDispatch()
 
   useEffect(() => {
     const checkAuth = async () => {
       const user = await authUtils.isAuthenticated();
       if (!user) {
         navigate('/login');
+      } else {
+        dispatch(setUser(user))
       }
     };
     checkAuth();
-  }, [navigate]);
+  }, [navigate, dispatch]);
   return (
     <div>
       <Box sx={{ display: 'flex' }}>
