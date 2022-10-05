84d77693d74127576b781e9c66957dda48579e8a
OKAMOTO YUKI <okayu110108@gmail.com>
2022-10-05 20:59:42 +0900

    Create update function

--- client/src/pages/Memo.jsx
+++ client/src/pages/Memo.jsx
@@ -1,9 +1,60 @@
 import { Box, IconButton, TextField } from '@mui/material';
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import StarBorderOutlinedIcon from '@mui/icons-material/StarBorderOutlined';
 import DeleteOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
+import memoApi from '../api/memoApi';
+import { useState } from 'react';
 
 const Memo = () => {
+  const { memoId } = useParams();
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+
+  useEffect(() => {
+    const getMemo = async () => {
+      try {
+        const res = await memoApi.getOne(memoId);
+        // console.log(res);
+        setTitle(res.title);
+        setDescription(res.description);
+      } catch (error) {
+        alert(error);
+      }
+    };
+    getMemo();
+  }, [memoId]);
+
+  let timer
+  const timeout = 500
+
+  const updateTitle = async (e) => {
+    clearTimeout(timer)
+    const newTitle = e.target.value;
+    setTitle(newTitle)
+
+    timer = setTimeout(async () => {
+      try {
+        await memoApi.update(memoId, { title: newTitle})
+      } catch (error) {
+        alert(error)
+      }
+    }, timeout)
+  }
+  const updateDescription = async (e) => {
+    clearTimeout(timer)
+    const newDescription = e.target.value;
+    setDescription(newDescription)
+
+    timer = setTimeout(async () => {
+      try {
+        await memoApi.update(memoId, { description: newDescription})
+      } catch (error) {
+        alert(error)
+      }
+    }, timeout)
+  }
+
   return (
     <>
       <Box
@@ -23,6 +74,8 @@ const Memo = () => {
       <Box sx={{ padding: '10px 50px' }}>
         <TextField
           placeholder='無題'
+          value={title}
+          onChange={updateTitle}
           variant='outlined'
           fullWidth
           sx={{
@@ -33,6 +86,8 @@ const Memo = () => {
         />
         <TextField
           placeholder='追加'
+          value={description}
+          onChange={updateDescription}
           variant='outlined'
           fullWidth
           sx={{
--- server/src/v1/controllers/memo.js
+++ server/src/v1/controllers/memo.js
@@ -24,10 +24,30 @@ exports.getAll = async (req, res) => {
 
 exports.getOne = async (req, res) => {
   const { memoId } = req.params;
+
   try {
-    const memo = Memo.findOne({ user: req.user._id, _id: memoId });
-    res.status(200).json(memo);
+    const memo = await Memo.findOne({ user: req.user._id, _id: memoId });
     if (!memo) return res.status(404).json('メモが存在しません');
+    res.status(200).json(memo);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+exports.update = async (req, res) => {
+  const { memoId } = req.params;
+  const { title, description } = req.body;
+
+  try {
+    if (title === '') req.body.title = '無題';
+    if (description === '')
+      req.body.description = 'ここに自由に記入してください';
+    const memo = await Memo.findOne({ user: req.user._id, _id: memoId });
+    if (!memo) return res.status(404).json('メモが存在しません');
+    const updatedMemo = await Memo.findByIdAndUpdate(memoId, {
+      $set: req.body,
+    });
+    res.status(200).json(updatedMemo);
   } catch (error) {
     res.status(500).json(error);
   }
